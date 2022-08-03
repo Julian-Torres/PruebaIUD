41fa80bcbb44d191d829ccd3dd9e97c4b6df2ae0
Julian Torres <julian.torres@est.iudigital.edu.co>
2022-08-02 23:28:02 -0500

    Compactado

--- pruebauno.py
+++ pruebauno.py
@@ -11,10 +11,10 @@ P=int(input("Ingresa la cantidad de permutaciones: ")) 						#cantidad de pasos
 #print (C[1:-1]) 															#lista de control
 
 for j in range(P): 		
-	D=[]            														#lista de cambios													
-	for i in range(len(C)):													#llenar lista de cambios
-		D.append(C[i])
-		
+#	D=[]            														#lista de cambios													
+#	for i in range(len(C)):													#llenar lista de cambios
+#		D.append(C[i])
+	D=C		
 	for i in range(1,len(C)-1):												#hacer el proceso
 #		print("paso ",i)													#control
 #		print ("Anterior: ",C[i-1]," Actual: ",C[i]," Siguiente: ",C[i+1])	
@@ -24,7 +24,8 @@ for j in range(P):
 		else:																#anterior y sigueinte diferente =1
 #			print ("deberia ser ",1)
 			D[i]=1
-	for i in range(len(C)):													#lista de cambos pasa a ser la original
-		C[i]=D[i]
+#	for i in range(len(C)):													#lista de cambos pasa a ser la original
+#		C[i]=D[i]
+	C=D
 #	print (D[1:-1])
 print (C[1:-1])
